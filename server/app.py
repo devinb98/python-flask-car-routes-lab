71b6b00e888cfced5479a283d2edc8accfb6d4e2
devin <devinb98@gmail.com>
2026-07-08 00:42:24 -0400

    added routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:model>')
+def model(model):
+    if model in existing_models:
+        return f'<h1>Flatiron {model} is in our fleet!</h1>'
+    else:
+        return f'<h1>No models called {model} exists in our catalog.</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
